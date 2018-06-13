4d29f17135c34e71f2aa5d57a481b5a9933e35b1
Marco Capitani <m.capitani@nextworks.it>
2018-06-13 12:01:55 +0200

    repo: update dep_setup, add install_libs and README

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Installation guide
+
+### Setup dependencies
+
+Run the `dep_setup.sh` script. Please check that everything has been correctly installed.
+
+Then run the `install_libs.sh` script giving as argument the desired parent folder for the libs repo.
+
+### Install the application
+
+Enter the `<repo-root>/SEBASTIAN` folder and run 
+```mvn clean package```
+
+A jar file (`Sebastian-<version>-<release-type>.jar`) will be generated in the `<repo-root>/SEBASTIAN/target` folder.
--- dep_setup.sh
+++ dep_setup.sh
@@ -7,9 +7,32 @@ if [[ $(id -u) != 0 ]]; then
 fi
 
 # Install deps
-apt-get install openjdk-8-jre -y
-apt-get install rabbitmq-server
-apt-get install postgresql -y
+
+# Java
+if ! type java > /dev/null; then
+    echo "Installing Java runtime"
+    apt-get install openjdk-8-jdk -y
+else
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo "Java version $version"
+    if [[ "$version" > "1.8" || "$version" == "1.8" ]]; then
+        echo "Java >=8 already present."
+    else
+        echo "Java is present, but in a version < 8."
+        echo "Please install Java 8 manually and configure it accordingly."
+    fi
+if ! type rabbitmq-server > /dev/null; then
+    echo "Installing rabbitmq"
+    apt-get install rabbitmq-server -y
+fi
+if ! type psql > /dev/null; then
+    echo "Installing postgresql"
+    apt-get install postgresql -y
+fi
+if ! type mvn > /dev/null; then
+    echo "Installing maven"
+    apt-get install maven
+fi
 
 # Start rabbit
 service rabbitmq-server start || true
@@ -17,6 +40,10 @@ service rabbitmq-server start || true
 # Configure & start postgres
 service postgresql start || true
 sudo -u postgres bash -c 'psql -c "ALTER USER postgres PASSWORD '"'postgres'"';"
-                          psql -c "CREATE DATABASE sebastian;"'
+                          psql -c "CREATE DATABASE timeo;"'
 service postgresql restart
+
+echo "****************"
+echo "Please run './install_libs.sh <FOLDER>' to complete the installation"
+echo "****************"
 
