e2c12d47a9923446d04893e7df1074355101dfb8
Marco Capitani <m.capitani@nextworks.it>
2018-07-05 17:50:02 +0200

    security: fix for incorrect role checking

--- SEBASTIAN/src/main/java/it/nextworks/nfvmano/sebastian/security/SebCorsFilter.java
+++ SEBASTIAN/src/main/java/it/nextworks/nfvmano/sebastian/security/SebCorsFilter.java
@@ -34,14 +34,14 @@ public class SebCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String origin = request.getHeader("Origin");
-        if (origin == null) {
-            origin = "*";
+        if (!(origin == null)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Max-Age", "180");
+            filterChain.doFilter(servletRequest, response);
         }
-        response.setHeader("Access-Control-Allow-Origin", origin);
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180");
         filterChain.doFilter(servletRequest, response);
     }
 
