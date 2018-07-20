8e091997927d3f19595b23d9d49e8c5aa9afe67d
Marco Capitani <m.capitani@nextworks.it>
2018-07-20 15:59:42 +0200

    docker: fixes for logging driver

--- make-docker-container.sh
+++ make-docker-container.sh
@@ -1,3 +1,9 @@
 cwd=$(pwd)
 
-docker run -d --name seb-tests -p 80:80 -p 5432:5432 -p 8082 -v "$cwd"/SEBASTIAN:/www -v "$cwd"/SEBASTIAN/target:/sebastian nextworks/sebastian-tests
+if docker ps -a | awk '{print $NF}' | grep seb-tests > /dev/null; then
+    echo "Deleting previous container."
+    docker rm seb-tests || exit 1
+fi
+
+docker run -d --name seb-tests -p 80:80 -p 5432:5432 -p 8082:8082 -v "$cwd"/SEBASTIAN_WEB_GUI:/www -v "$cwd"/SEBASTIAN/target:/sebastian nextworks/sebastian-tests
+docker cp docker/application.properties seb-tests:/sebastian/application.properties
