925a7d87bf9599d55a0f93c1ba543bfa5af400fb
Marco Capitani <m.capitani@nextworks.it>
2018-07-31 08:42:43 +0000

    test: add cirros-test VS scripts

---
+++ SEBASTIAN/test/cirros-test/create_tenant.sh
@@ -0,0 +1,1 @@
+curl -v -d @create_tenant.json -X POST http://localhost:8082/vs/admin/group/media_provider/tenant --header "Content-Type:application/json" -b /tmp/a_c
---
+++ SEBASTIAN/test/cirros-test/instantiate_vs1.sh
@@ -0,0 +1,1 @@
+curl -v -d @instantiate_vs1.json -X POST http://localhost:8082/vs/basic/vslcm/vs --header "Content-Type:application/json" -b /tmp/seb_cookie
