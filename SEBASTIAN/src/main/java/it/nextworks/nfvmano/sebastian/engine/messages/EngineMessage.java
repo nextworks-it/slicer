75628ffff0e5986db195ff18152f42b2bd5590ee
j.brenes <j.brenes@nextworks.it>
2019-05-31 13:59:49 +0200

    fixed arbitration missing message type

--- SEBASTIAN/src/main/java/it/nextworks/nfvmano/sebastian/engine/messages/EngineMessage.java
+++ SEBASTIAN/src/main/java/it/nextworks/nfvmano/sebastian/engine/messages/EngineMessage.java
@@ -36,6 +36,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 	@Type(value = TerminateNsiRequestMessage.class, 	name = "TERMINATE_NSI_REQUEST"),
 	@Type(value = NotifyNsiStatusChange.class, 	name = "NOTIFY_NSI_STATUS_CHANGE"),
 	@Type(value = NotifyNfvNsiStatusChange.class, 	name = "NOTIFY_NFV_NSI_STATUS_CHANGE"),
+	@Type(value = CoordinateVsiRequest.class, 	name = "COORDINATE_VSI_REQUEST"),
 })
 public abstract class EngineMessage {
 
