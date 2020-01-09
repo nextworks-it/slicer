12c7ba22f10a7d1faf8cdb55c0e0761599090566
p.piscione <p.piscione@nextworks.it>
2020-01-09 14:15:26 +0100

    removed VS blueprint IM, catalogue_interface and catalogue directories and from pom file into sebastian

--- SEBASTIAN/VS_BLUEPRINTS_CATALOGUE/src/main/java/it/nextworks/nfvmano/catalogue/blueprint/repo/ServiceConstraintsRepository.java
+++
@@ -1,9 +0,0 @@
-package it.nextworks.nfvmano.catalogue.blueprint.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.nextworks.nfvmano.catalogue.blueprint.elements.ServiceConstraints;
-
-public interface ServiceConstraintsRepository extends JpaRepository<ServiceConstraints, Long> {
-
-}
--- SEBASTIAN/VS_BLUEPRINTS_CATALOGUE/src/main/java/it/nextworks/nfvmano/catalogue/blueprint/repo/VsComponentRepository.java
+++
@@ -1,9 +0,0 @@
-package it.nextworks.nfvmano.catalogue.blueprint.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.nextworks.nfvmano.catalogue.blueprint.elements.VsComponent;
-
-public interface VsComponentRepository extends JpaRepository<VsComponent, Long> {
-
-}
