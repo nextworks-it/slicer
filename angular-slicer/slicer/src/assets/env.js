d4b5e0329d1f6b0516528d6a51692a4dfc1ecfd4
j.brenes <j.brenes@nextworks.it>
2021-06-01 18:01:32 +0200

    modified gui to use environment variables

---
+++ angular-slicer/slicer/src/assets/env.js
@@ -0,0 +1,7 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["baseUrl"] = "https://api.myapp.com";
+  window["env"]["debug"] = true;
+})(this);
